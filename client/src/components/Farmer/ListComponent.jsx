e407f3eda1940467257cab01ab14439904887088
Harshit Bamotra <harshitbamotra.01@gmail.com>
2023-09-15 00:57:07 +0530

    Repairing past mistakes

--- client/src/components/Farmer/ListComponent.jsx
+++ client/src/components/Farmer/ListComponent.jsx
@@ -8,7 +8,7 @@ export default function ListComponent(){
                 <div className='plan-max-time'>maximum time: 7 months</div>
             </div>
             <div className='list-name'>provider: Harshit Bamotra</div>
-            <div className='list-price'>30$ per kg</div>
+            <div className='list-price'><span style={{fontWeight:"700"}}>Cost: </span>30$ per kg</div>
         </div>
     )
 }
